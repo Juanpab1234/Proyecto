c26c85b3a16afd164cde8e15aeba29354f15fa8d
Cata142 <cvilla142003@gmail.com>
2024-05-16 17:41:50 -0500

    Actualizacion de expo, correcion de dislexia y intento de navegacion

--- Proyecto/App.js
+++ Proyecto/App.js
@@ -1,20 +1,33 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
+import Principal from './Principal';
+import Login from './Login';
+import Registro from './Registro';
+import inversion from './inversion';
+import invinter from './invinter';
+import invnac from './invnac';
+import matpago from './matpago';
 
-export default function invnac() {
+export default function App() {
+  const Stack = createNativeStackNavigator();
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaProvider>
+      <NavigationContainer>
+        <SafeAreaView style={{ flex: 1 }}>
+          <Stack.Navigator>
+          <Stack.Screen name='Principal' component={Principal} />
+            <Stack.Screen name='Login' component={Login} />
+            <Stack.Screen name='Registro' component={Registro} />
+            <Stack.Screen name='Inversion' component={inversion} />
+            <Stack.Screen name='Invinter' component={invinter} />
+            <Stack.Screen name='Invnac' component={invnac} />
+            <Stack.Screen name='Matpago' component={matpago} />
+          </Stack.Navigator>
+        </SafeAreaView>
+      </NavigationContainer>
+    </SafeAreaProvider>
   );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+};
--- Proyecto/Principal.js
+++ Proyecto/Principal.js
@@ -1,83 +1,77 @@
 import { StatusBar } from 'expo-status-bar';
-import { ScrollView, StyleSheet, Text, View, Image} from 'react-native';
+import { ScrollView, StyleSheet, Text, View, Image, Button } from 'react-native';
 
-export default function invnac() {
+export default function Principal({ navigation }) {
   return (
-    
     <ScrollView contentContainerStyle={styles.container}>
-      
-    <View style={styles.Bienvenido}>
-    <Image
-        style={styles.Logo}
-        source={{
-          uri: "https://live.staticflickr.com/65535/53649348379_4271064c62_n.jpg",
-        }}
-      />
-    </View>
-    <Text style={styles.text}>BIENVENIDOS</Text>
-    <View style={styles.empresa}>
-    <Text style={styles.text2}>LOGIN</Text>
-    <Text style={styles.text1}>Iniciar</Text>
-    </View>
-    <View style={styles.empresa}>
-    <Text style={styles.text2}>REGISTRAR</Text>
-    <Text style={styles.text1}>Nuevo Usuario</Text>
-    </View>
+      <View style={styles.Bienvenido}>
+        <Image
+          style={styles.Logo}
+          source={{
+            uri: "https://live.staticflickr.com/65535/53649348379_4271064c62_n.jpg",
+          }}
+        />
+      </View>
+      <Text style={styles.text}>BIENVENIDOS</Text>
+      <View style={styles.empresa}>
+        <Button 
+          title="Login"
+          onPress={() => navigation.navigate('Login')}
+        />
+      </View>
+      <View style={styles.empresa}>
+        <Text style={styles.text2}>REGISTRAR</Text>
+        <Text style={styles.text1}>Nuevo Usuario</Text>
+      </View>
     </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex:1,
+    flex: 1,
     backgroundColor: '#000',
     alignItems: 'center',
     justifyContent: 'center',
   },
-  Bienvenido:{
-    backgroundColor:'white',
+  Bienvenido: {
+    backgroundColor: 'white',
     height: 160,
-    width:250,
-    justifyContent:'center',
-    borderRadius:20,
-    marginBottom:10,
+    width: 250,
+    justifyContent: 'center',
+    borderRadius: 20,
+    marginBottom: 10,
   },
   text: {
     textAlign: 'center',
-    fontSize:45,
-    color:'#30ded5',
-  },
-
-empresa:{
-  textAlign: 'center',
-  backgroundColor:'white',
+    fontSize: 45,
+    color: '#30ded5',
+  },
+  empresa: {
+    textAlign: 'center',
+    backgroundColor: 'white',
     height: 55,
-    width:200,
-    borderRadius:30,
+    width: 200,
+    borderRadius: 30,
     marginLeft: 45,
-    marginTop:40,
-},
-text2: {
-  tectAling: 'center',
-  fontSize:25,
-  color:'#000',
-  marginLeft: 35,
-  },
-text1: {
-  textAlign: 'center',
-  fontSize:20,
-  color:'gray',
-  marginLeft:30,
-},
-text3: {
-  textAlign: 'center',
-  fontSize:20,
-  color:'gray',
-  marginLeft:30,
-},
-Logo: {
-  width: 250,
-  height: 160,
-  borderRadius:19,
-},
-  });
+    marginTop: 40,
+  },
+  text2: {
+    textAlign: 'center',
+    fontSize: 25,
+    color: '#000',
+    marginLeft: -3,
+  },
+  text1: {
+    textAlign: 'center',
+    fontSize: 18,
+    color: 'gray',
+    marginLeft: 4,
+    marginTop: 10,
+  },
+  Logo: {
+    width: 250,
+    height: 160,
+    borderRadius: 19,
+  },
+});
